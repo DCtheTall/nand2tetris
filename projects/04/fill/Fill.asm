69d694bd239c2b619ee64b096218372b72a685cc
DCtheTall <dylancutler@google.com>
2021-06-24 21:45:41 -0400

    improve Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -31,11 +31,21 @@
   @KBD
   D=M
   // if D == 0 jump to WHITE
-  @WHITE_LOOP
+  @WHITE
   D;JEQ
 
+  // otherwise set color to -1
+  @color
+  M=-1
+  @INNER_LOOP
+  0;JMP
+
+  (WHITE)
+    @color
+    M=0
+
   // Color screen black
-  (BLACK_LOOP)
+  (INNER_LOOP)
     // If i == 8192 then continue OUTER_LOOP
     @i
     D=M
@@ -44,42 +54,25 @@
     @OUTER_LOOP
     D;JEQ
 
-    // Color screen black at current RAM address
-    @i
-    D=M
+    // Store the current pixel address in a variable
     @SCREEN
-    A=A+D
-    M=-1
+    D=A
+    @i
+    D=D+M
+    @addr
+    M=D
+
+    // Set the pixel to @color
+    @color
+    D=M
+    @addr
+    A=M
+    M=D
 
     // Increment i
     @i
     M=M+1
 
     // Continue coloring screen black
-    @BLACK_LOOP
-    0;JMP
-
-  // Color screen white
-  (WHITE_LOOP)
-    // If i == 8192 then continue OUTER_LOOP
-    @i
-    D=M
-    @8192
-    D=D-A
-    @OUTER_LOOP
-    D;JEQ
-
-    // Color screen white at current RAM address
-    @i
-    D=M
-    @SCREEN
-    A=A+D
-    M=0
-
-    // Increment i
-    @i
-    M=M+1
-
-    // Continue coloring screen white
-    @WHITE_LOOP
+    @INNER_LOOP
     0;JMP
