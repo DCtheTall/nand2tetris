a84d581ad99d82345f11bee87b189a1fe0a8af70
DCtheTall <dylancutler@google.com>
2021-07-18 15:23:44 -0400

    pass StaticTest in project 7

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -101,10 +101,7 @@ def TranslateVMtoASM(vm_tokens: List[List[str]], file_label) -> str:
           'M=M+D' if op == 'add' else 'M=M-D',
       ])
     elif op == 'neg':
-      result.extend([
-          '@SP',
-          'M=-M',
-      ])
+      result.extend(['@SP', 'M=-M'])
     # TODO rest of ops
     else:
       raise SyntaxError('Unexpected operation: {}'.format(op))
@@ -118,50 +115,47 @@ def LoadValueToD(segment: str, offset: int, file_label: str) -> List[str]:
     return ['@{}'.format(offset), 'D=A']
   if segment == 'temp':
     return ['@{}'.format(5 + offset), 'D=M']
-
-  result = []
-  
   if segment in SEGMENT_POINTERS:
-    result.append('@{}'.format(SEGMENT_POINTERS[segment]))
-  elif segment == 'static':
-    result.append('@{}.{}'.format(file_label, offset))
-  elif segment == 'pointer':
-    result.append('@{}'.format('THAT' if offset else 'THIS'))
-  else:
+    return [
+        '@{}'.format(SEGMENT_POINTERS[segment]),
+        'D=M',
+        '@{}'.format(offset),
+        'A=A+D',
+        'D=M',
+    ]
+  if segment == 'static':
+    return [
+        '@{}.{}'.format(file_label, offset),
+        'D=M',
+    ]
+  if segment != 'pointer':
     raise SyntaxError('Unexpected segment: {}'.format(segment))
-
-  if segment in ['static', 'pointer']:
-    result.extend(['A=M', 'D=M'])
-    return result
-
-  result.extend([
-      'D=M',
-      '@{}'.format(offset),
-      'A=A+D',
-      'D=M',
-  ])
-  return result
+  # TODO pointer
+  return []
 
 
 def LoadAddressIntoR15(segment: str, offset: int, file_label: str) -> List[str]:
   """Load the address of the pointer determined by (segment, offset) into RAM[15]."""
   result = []
-
-  if segment in SEGMENT_POINTERS:
-    result.append('@{}'.format(SEGMENT_POINTERS[segment]))
+  if segment == 'temp':
+    result = ['@{}'.format(5 + offset), 'D=A']
+  elif segment in SEGMENT_POINTERS:
+    result = [
+        '@{}'.format(SEGMENT_POINTERS[segment]),
+        'D=M',
+        '@{}'.format(offset),
+        'D=D+A',
+    ]
   elif segment == 'static':
-    result.append('@{}'.format('{}.{}'.format(file_label, offset)))
+    result = [
+        '@{}.{}'.format(file_label, offset),
+        'D=A',
+    ]
   elif segment == 'pointer':
-    result.append('@{}'.format('THAT' if offset else 'THIS'))
-  elif segment == 'temp':
-    result.append('@{}'.format(5 + offset))
+    # TODO pointer
+    pass
   else:
     raise SyntaxError('Unexpected segment: {}'.format(segment))
-
-  result.append('D=A' if segment == 'temp' else 'D=M')
-
-  if segment not in ['static', 'pointer', 'temp']:
-    result.extend(['@{}'.format(offset), 'D=D+A'])
 
   result.extend(['@R15', 'M=D'])
   return result
